d3cf057d1ff55c872114db8cbf064ec16442a2b5
DinhHoa14 <hoamndse173090@fpt.edu.vn>
2025-02-11 21:05:11 +0700

    Format

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path'; // Dùng để xử lý alias
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // Dùng để xử lý alias
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@pages': path.resolve(__dirname, './src/page'),
-      '@containers': path.resolve(__dirname, './src/container'),
-      '@layouts': path.resolve(__dirname, './src/layout'),
-      '@routes': path.resolve(__dirname, './src/routes'),
-      '@components': path.resolve(__dirname, './src/components'), // Thêm alias cho components nếu cần
-      '@utils': path.resolve(__dirname, './src/utils'), // Alias cho các tiện ích
-      '@src': path.resolve(__dirname, './src'), // Alias cho các tiện ích
+      "@pages": path.resolve(__dirname, "./src/page"),
+      "@containers": path.resolve(__dirname, "./src/container"),
+      "@layouts": path.resolve(__dirname, "./src/layout"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@components": path.resolve(__dirname, "./src/components"), // Thêm alias cho components nếu cần
+      "@utils": path.resolve(__dirname, "./src/utils"), // Alias cho các tiện ích
+      "@src": path.resolve(__dirname, "./src"), // Alias cho các tiện ích
     },
   },
-  base: "/Spring2025SWD392_NET1709_thinh_fe/"
-})
+  base: "/Spring2025SWD392_NET1709_thinh_fe/",
+});
