93f0bd534280cb8339c5b0ad0c400fccc7bd8ca8
ThinhPhoenix <laithinh1310@gmail.com>
2025-02-15 09:57:22 +0700

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // Dùng để xử lý alias
+import path from "path";
 import { fileURLToPath } from "url";
+import process from "node:process";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@pages": path.resolve(__dirname, "./src/page"),
-      "@containers": path.resolve(__dirname, "./src/container"),
-      "@layouts": path.resolve(__dirname, "./src/layout"),
-      "@routes": path.resolve(__dirname, "./src/routes"),
-      "@components": path.resolve(__dirname, "./src/components"), // Thêm alias cho components nếu cần
-      "@utils": path.resolve(__dirname, "./src/utils"), // Alias cho các tiện ích
-      "@src": path.resolve(__dirname, "./src"), // Alias cho các tiện ích
+export default defineConfig(({ mode }) => {
+  // Load biến môi trường từ .env
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@pages": path.resolve(__dirname, "./src/page"),
+        "@containers": path.resolve(__dirname, "./src/container"),
+        "@layouts": path.resolve(__dirname, "./src/layout"),
+        "@routes": path.resolve(__dirname, "./src/routes"),
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@utils": path.resolve(__dirname, "./src/utils"),
+        "@src": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  base: "/Spring2025SWD392_NET1709_thinh_fe/",
+    base: env.VITE_BASE_PATH || "/",
+  };
 });
